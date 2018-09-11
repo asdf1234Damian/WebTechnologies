c60802b02dccc73cf7954888ae5345dbef61da33
asdf1234Damian <asdf1234damian@gmail.com>
2018-09-11 07:17:53 -0500

    Proyecto 11 Sept 7

--- Laboratorio03/src/java/JavaServlet/Servlet2.java
+++ Laboratorio03/src/java/JavaServlet/Servlet2.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpSession;
 
 public class Servlet2 extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)  
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
